dc9ce9c03c785e2446eb427e95ba076f23cedc05
michellejyao <itsmichelle.j.y@gmail.com>
2024-02-11 18:43:29 -0500

    last week code

--- Crescendo2024/src/main/java/frc/robot/Constants.java
+++ Crescendo2024/src/main/java/frc/robot/Constants.java
@@ -16,4 +16,29 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public final static class ArmSubsystemConstants{
+    // CAN ID
+    public final static int ArmPivotLeaderCAN = 0;
+    public final static int ArmPivotFollowerCAN = 1;
+
+    // Simulation Constants
+    public final static double restDegreesFromHorizontal = 75;
+
+     // OPERATIONAL
+     public final static double ArmShoulderRotatePower = 0.5; // testing, not final
+     public final static double ArmShoulderStop = 0.0; // testing, not final
+     public final static double ArmShoulderFeedForwardMin = 0.26; // final and tested
+     public final static double ArmShoulderFeedForwardMax = 0.5; // final and tested
+     public final static double ArmShoulderFeedForwardDifference = ArmShoulderFeedForwardMax - ArmShoulderFeedForwardMin;
+     public static final double ShoulderBufferTimeInSeconds = 0.75;
+
+     // REAL WORLD CONSTANTS
+    public final static double angleAdjustmentDegrees = 71.57;
+    public final static double angleAdjustmentRadians = Units.degreesToRadians(angleAdjustmentDegrees);
+    public final static double EncoderToOutputRatio = 0.5;
+    public final static double restDegreesFromHorizontal = 75;
+    public final static int EncoderCPR = 4096;
+    public final static double ticksPerDegree = EncoderCPR / 360;
+  }
 }
---
+++ Crescendo2024/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,222 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+import com.ctre.phoenix.motorcontrol.TalonSRXSimCollection;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ArmSubsystemConstants;
+import frc.robot.FireBirdsUtils;
+
+public class ArmSubsystem extends SubsystemBase {
+  // Declaring motor controllers 
+  private final WPI_TalonSRX armPivotLeader = new WPI_TalonSRX(ArmSubsystemConstants.ArmPivotLeaderCAN);
+  private final WPI_VictorSPX armPivotFollower = new WPI_VictorSPX(ArmSubsystemConstants.ArmPivotFollowerCAN);
+  
+  private final static FireBirdsUtils util = new FireBirdsUtils();
+
+  // Target angles for arm
+  private static double targetPositionForAmp;
+  private static double targetPositionForSpeaker;
+
+  /** Object of a simulated arm **/
+  private final SingleJointedArmSim armSim = new SingleJointedArmSim(DCMotor.getCIM(2), 
+  139.78, 6.05, 1, 
+  Units.degreesToRadians(-ArmSubsystemConstants.restDegreesFromHorizontal),
+  6,true, 0);
+
+  // Initializing the simulation of TalonSRX
+  private final TalonSRXSimCollection armPivotLeaderSim = armPivotLeader.getSimCollection();
+
+  // Create a Mechanism2d display of an Arm with a fixed ArmTower and moving Arm.
+  private final Mechanism2d m_mech2d = new Mechanism2d(80, 80); // the overall arm
+  private final MechanismRoot2d m_armPivot = m_mech2d.getRoot("ArmPivot", 30, 30); // pivot point
+  private final MechanismLigament2d m_armTower = m_armPivot.append(new MechanismLigament2d("ArmTower", 30, -90));
+  private final MechanismLigament2d m_arm = m_armPivot.append(
+      new MechanismLigament2d(
+          "Arm",
+          30,
+          Units.radiansToDegrees(armSim.getAngleRads()),
+          6,
+          new Color8Bit(Color.kAliceBlue)));
+
+  // Creates a new ArmSubsystem
+  public ArmSubsystem() {
+    // idk what this does IDK WHAT THIS DOESSSSS
+    armPivotLeader.configFactoryDefault();
+    armPivotFollower.configFactoryDefault();
+
+    armPivotFollower.follow(armPivotLeader);
+    armPivotLeader.setInverted(false);
+    armPivotFollower.setInverted(InvertType.FollowMaster);
+    armPivotLeader.configVoltageCompSaturation(12,0);
+
+    // Setting the value of P in PID control
+    armPivotLeader.config_kP(0, 1.5);
+
+    // Setting the velocity and acceleration of the motors
+    armPivotLeader.configMotionCruiseVelocity(0);
+    armPivotLeader.configMotionAcceleration(2);
+
+    // Put Mechanism 2d to SmartDashboard
+    SmartDashboard.putData("Arm Sim", m_mech2d);
+    m_armTower.setColor(new Color8Bit(Color.kBlue));
+
+    // Configure the encoder
+    armPivotLeader.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0,
+        0);
+
+    if (RobotBase.isSimulation()) {
+      armPivotLeader.setInverted(false);
+      armPivotLeader.setSensorPhase(false);
+    } 
+    else {
+      armPivotLeader.setInverted(false);
+      armPivotLeader.setSensorPhase(true);
+    }
+
+    // Configuring the limit switch
+    armPivotLeader.configReverseLimitSwitchSource(LimitSwitchSource.FeedbackConnector,
+      LimitSwitchNormal.NormallyOpen);
+
+    armPivotLeader.configForwardLimitSwitchSource(LimitSwitchSource.Deactivated, 
+      LimitSwitchNormal.NormallyOpen);
+
+    // If the robot arm is already at the target position, it will set the target position to current position
+    if(getPosition()==targetPositionForAmp){
+      targetPositionForAmp = getPosition();
+    }
+    if(getPosition()==targetPositionForSpeaker){
+      targetPositionForSpeaker = getPosition();
+    }
+  }
+
+  // Getter methods
+
+  // Getting the position of the motor
+  public double getPosition() {
+    // Absolute position gets the location of the arm in ticks (4096 per revolution)
+    return armPivotLeader.getSelectedSensorPosition();
+  }
+
+  // Setter methods
+  
+  // Gives power
+  public void setPower(double power){
+    armPivotLeader.set(power);
+  }
+
+  // Input two values that will be used for feedforward
+  public void setPosition(double demand0, double demand1) {
+    armPivotLeader.set(ControlMode.Position, demand0, DemandType.ArbitraryFeedForward, 0);
+  }
+
+  public void setNewAmpTargetPosition(double newAmpTargetPosition) {
+    targetPositionForAmp = newAmpTargetPosition;
+  }
+
+  public void setNewSpeakerTargetPosition(double newSpeakerTargetPosition) {
+    targetPositionForAmp = newSpeakerTargetPosition;
+  }  
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    if (armPivotLeader.isRevLimitSwitchClosed() == 1) {
+      armPivotLeader.setSelectedSensorPosition(0);
+
+    }
+
+    double currentPosition = getPosition(); //in raw sensor units
+
+    // Where does this equation come from? HOW IS THIS EQUATION FORMED????
+    double adjusted_feedForward = (ArmSubsystemConstants.ArmShoulderFeedForwardMin 
+    * Math.cos(util.CTRESensorUnitsToRads(targetPositionForAmp, ArmSubsystemConstants.EncoderCPR)-
+            ArmSubsystemConstants.angleAdjustmentRadians);
+
+    SmartDashboard.putNumber("Current Arm Position: ", currentPosition);
+    SmartDashboard.putNumber("Target Position", targetPositionForAmp);
+    SmartDashboard.putNumber("Leader Voltage", armPivotLeader.getMotorOutputVoltage());
+    SmartDashboard.putNumber("Adjusted feedforward", adjusted_feedForward);
+    SmartDashboard.putNumber("Shoulder Current (A)", armPivotLeader.getSupplyCurrent());
+    SmartDashboard.putNumber("Shoulder Error", armPivotLeader.getClosedLoopError(0));
+    SmartDashboard.putNumber("Shoulder Position: ",  getPosition());
+    SmartDashboard.putBoolean("Is arm above level 1", isArmAboveLevel1());
+
+    if (RobotBase.isSimulation()) {
+      armPivotLeader.set(ControlMode.MotionMagic, targetPositionForAmp, DemandType.ArbitraryFeedForward,
+          adjusted_feedForward);
+    } 
+    else {
+      armPivotLeader.set(ControlMode.MotionMagic, targetPositionForAmp*2, DemandType.ArbitraryFeedForward,
+          adjusted_feedForward);
+    }
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // In this method, we update our simulation of what our arm is doing
+    // First, we set our "inputs" (voltages)
+    armSim.setInput(armPivotLeaderSim.getMotorOutputLeadVoltage());
+    // Next, we update it. The standard loop time is 20ms.
+    armSim.update(0.020);
+
+    armPivotLeaderSim
+        .setQuadratureRawPosition(util.radsToCTRESensorUnits(armSim.getAngleRads(), ArmSubsystemConstants.EncoderCPR));
+
+    m_arm.setAngle(Math.toDegrees(armSim.getAngleRads()));
+
+    // Zero the limit switch in simulation
+    if (armSim.getAngleRads() == Units.degreesToRadians(-ArmSubsystemConstants.restDegreesFromHorizontal)) {
+
+      armPivotLeaderSim.getSensorCollection().setQuadraturePosition(0, 0);
+    }
+
+    armPivotLeaderSim.setAnalogPosition(util.radsToCTRESensorUnits(armSim.getAngleRads(), 4096));
+
+  }
+}
