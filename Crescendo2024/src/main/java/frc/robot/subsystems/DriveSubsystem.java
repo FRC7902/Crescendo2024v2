42e66beff5c38477d49c31c5f71a65538786553b
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-02-17 01:22:32 -0500

    camera + pathplanner

--- Crescendo2024/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ Crescendo2024/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,13 +7,20 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
+
+import org.photonvision.PhotonCamera;
+
 import com.kauailabs.navx.frc.AHRS;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.util.ReplanningConfig;
 
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.AnalogGyro;
@@ -65,9 +72,22 @@ public class DriveSubsystem extends SubsystemBase {
   private AnalogGyroSim m_gyroSim;
   public DifferentialDrivetrainSim m_driveTrainSim;
   private Field2d m_fieldSim;
+  private PhotonCamera m_camera;
+
+  private final DifferentialDriveKinematics m_kinematics = new DifferentialDriveKinematics(DriveConstants.trackWidthInches);
+  private final DifferentialDrivePoseEstimator m_poseEstimator = new DifferentialDrivePoseEstimator(
+    m_kinematics,
+    ahrs.getRotation2d(),
+    m_leftEncoder.getPosition(),
+    m_rightEncoder.getPosition(),
+    new Pose2d(),
+    VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5)),//state std devs
+    VecBuilder.fill(0.5, 0.5, Units.degreesToRadians(30))//cam std devs
+  );
 
   /** Creates a new DriveSubsystem. */
-  public DriveSubsystem() {
+  public DriveSubsystem(PhotonCamera camera) {
+    m_camera = camera;
 
     // m_leftLeaderMotor.restoreFactoryDefaults(); //Safety precautions, but
     // reccomended
@@ -117,7 +137,6 @@ public class DriveSubsystem extends SubsystemBase {
         new Pose2d(1, 1, new Rotation2d()));
     }
 
-
     m_driveTrainSim = DifferentialDrivetrainSim.createKitbotSim( // CHANGE AS NEEDED!!
         KitbotMotor.kDualCIMPerSide,
         KitbotGearing.k10p71,
@@ -155,18 +174,32 @@ public class DriveSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
 
-
     if(Robot.isSimulation()){
       m_odometry.update(
         m_gyro.getRotation2d(),
         m_leftEncoderObj.getDistance(),
-        m_rightEncoderObj.getDistance());
-    }else{
-      m_odometry.update(
-        m_gyro.getRotation2d(),
-        m_leftEncoder.getPosition(),
-        m_rightEncoder.getPosition()
+        m_rightEncoderObj.getDistance()
       );
+    }else{
+      if(m_camera.getLatestResult().hasTargets()){
+        updatePoseFromCamera(m_leftEncoder.getPosition(), m_rightEncoder.getPosition());
+        m_odometry.resetPosition(
+          ahrs.getRotation2d(), 
+          m_leftEncoder.getPosition(),
+          m_rightEncoder.getPosition(),
+          new Pose2d(
+            m_poseEstimator.getEstimatedPosition().getX(),
+            m_poseEstimator.getEstimatedPosition().getY(),
+            m_poseEstimator.getEstimatedPosition().getRotation()
+          )
+        );
+      }else{
+        m_odometry.update(
+          ahrs.getRotation2d(),
+          m_leftEncoder.getPosition(),
+          m_rightEncoder.getPosition()
+        );
+      }
     }
 
     // sim
@@ -200,6 +233,20 @@ public class DriveSubsystem extends SubsystemBase {
 
   }
 
+  public void updatePoseFromCamera(double leftDist, double rightDist){
+    m_poseEstimator.update(ahrs.getRotation2d(), leftDist, rightDist);
+
+    var res = m_camera.getLatestResult();
+
+    if(res.hasTargets()){
+      var imageCaptureTime = res.getTimestampSeconds();
+      var camToTargetTrans = res.getBestTarget().getBestCameraToTarget();
+      var camPose = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField().getTagPose(res.getBestTarget().getFiducialId()).get().transformBy(camToTargetTrans.inverse());
+    
+      m_poseEstimator.addVisionMeasurement(camPose.toPose2d(), imageCaptureTime);
+    }
+  }
+
   public void driveArcade(double xForward, double zRotation) {
     m_drive.arcadeDrive(xForward, zRotation);
   }
@@ -210,13 +257,10 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   public void driveSpeeds(ChassisSpeeds speeds) {
-
     this.driveArcade(speeds.vxMetersPerSecond / 4.379976, speeds.omegaRadiansPerSecond / 4.379976);
-
   }
 
   public ChassisSpeeds getWheelSpeeds() {
-
     if (Robot.isSimulation()) {
       return new ChassisSpeeds(m_leftEncoderObj.getRate(), m_rightEncoderObj.getRate(),
           m_gyro.getRate());
@@ -224,7 +268,6 @@ public class DriveSubsystem extends SubsystemBase {
       return new ChassisSpeeds(m_leftEncoder.getVelocity(), m_rightEncoder.getVelocity(),
           Units.degreesToRadians(m_gyro.getRate()));
     }
-
   }
 
   public void turn(double amount) {
@@ -232,12 +275,12 @@ public class DriveSubsystem extends SubsystemBase {
     m_rightLeaderMotor.set(-amount);
   }
 
-  public double getPositionSim() {
-    return m_rightEncoderObj.getDistance();
-  }
-
   public double getPosition() {
-    return m_rightEncoder.getPosition();
+    if(Robot.isSimulation()){
+      return m_rightEncoderObj.getDistance();
+    }else{
+      return m_rightEncoder.getPosition();
+    }
   }
 
   public void resetEncoders() {
@@ -270,11 +313,11 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   public double getHeading() {// -180 to 180
-    return Math.IEEEremainder(ahrs.getAngle(), 360);
-  }
-
-  public double getHeadingSim() {
-    return Math.IEEEremainder(m_gyro.getAngle(), 360);
+    if(Robot.isSimulation()){
+      return Math.IEEEremainder(m_gyro.getAngle(), 360);
+    }else{
+      return Math.IEEEremainder(ahrs.getAngle(), 360);
+    }
   }
 
   public double modAngle(double angle) {
