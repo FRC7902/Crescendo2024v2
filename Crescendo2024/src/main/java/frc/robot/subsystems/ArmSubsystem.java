942fd2129229954c89025928daa62d6f6ef1effb
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-02-16 20:37:57 -0500

    review2

--- Crescendo2024/src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ Crescendo2024/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -24,7 +24,6 @@ import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmSubsystemConstants;
 import frc.robot.FireBirdsUtils;
