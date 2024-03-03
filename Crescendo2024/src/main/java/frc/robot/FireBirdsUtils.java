0eca0fc0950fbf4f4ce1301d6745b8bc889619d4
parivansjalia <74889163+parivansjalia@users.noreply.github.com>
2024-03-03 16:46:28 -0500

    turn and drive command

--- Crescendo2024/src/main/java/frc/robot/FireBirdsUtils.java
+++ Crescendo2024/src/main/java/frc/robot/FireBirdsUtils.java
@@ -18,7 +18,7 @@ public class FireBirdsUtils {
     return (double) ((angleInSensorUnits / encoderCPR) * 360);
   }
 
-  public double[] setZeiglerNicholsConstants(double ku, double tu){
+  public double[] setZeiglerNicholsConstants(double ku, double tu) {
     double constants[] = new double[3];
     constants[0] = 0.6 * ku;
     constants[1] = 1.2 * ku / tu;
@@ -26,11 +26,34 @@ public class FireBirdsUtils {
     return constants;
   }
 
-    public double[] setZeiglerNicholsConstantsNoOvershoot(double ku, double tu){
+  public double[] setZeiglerNicholsConstantsNoOvershoot(double ku, double tu) {
     double constants[] = new double[3];
     constants[0] = 0.2 * ku;
     constants[1] = 0.4 * ku / tu;
     constants[2] = 0.0667 * ku * tu;
     return constants;
   }
+
+  public double TurnToPoint(double targetY, double targetX) {
+    double targetAngle = Math.atan(Math.abs((targetY / targetX)));
+    // Use addRequirements() here to declare subsystem dependencies.
+    if (targetY < 0) {
+      if (targetX < 0) {
+        targetAngle += 180;
+      } else {
+        targetAngle = 360 - targetAngle;
+      }
+    } else {
+      if (targetX < 0) {
+        targetAngle = 180 - targetAngle;
+      }
+    }
+    return targetAngle;
+  }
+
+  public double FindDistance(double targetY, double targetX) {
+    double targetDistance = Math.sqrt((targetY * targetY) + (targetX * targetX));
+    return targetDistance;
+  }
+
 }
---
+++ Crescendo2024/src/main/java/frc/robot/commands/teleopCommands/drive/TurnAndDrive.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.teleopCommands.drive;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.commands.teleopCommands.drive.TurnToAngle;
+import frc.robot.FireBirdsUtils;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class TurnAndDrive extends SequentialCommandGroup {
+  private double targetY;
+  private double targetX;
+  private DriveSubsystem m_drive;
+  private final FireBirdsUtils util = new FireBirdsUtils();
+
+  /** Creates a new TurnAndDrive. */
+  public TurnAndDrive(DriveSubsystem drive, double Y, double X) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    targetY = Y;
+    targetX = X;
+    double target = util.TurnToPoint(Y, X);
+    addCommands(
+        new TurnToAngle(m_drive, target, true));
+    new DriveToDistance(m_drive, util.FindDistance(targetY, targetX));
+  }
+}
