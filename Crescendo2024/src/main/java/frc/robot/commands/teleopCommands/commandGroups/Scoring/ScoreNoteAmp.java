f9e533f15bfbcb8aeb34c905443dd54677668513
Renyi-110 <142171702+Renyi-110@users.noreply.github.com>
2024-09-01 17:28:37 -0400

    timeout

--- Crescendo2024/src/main/java/frc/robot/commands/teleopCommands/commandGroups/Scoring/ScoreNoteAmp.java
+++ Crescendo2024/src/main/java/frc/robot/commands/teleopCommands/commandGroups/Scoring/ScoreNoteAmp.java
@@ -20,7 +20,7 @@ public class ScoreNoteAmp extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-        new AmpArmAndShooter(arm, shooter).withTimeout(5),
+        new AmpArmAndShooter(arm, shooter).withTimeout(2),
         new FeedNote(intake));
   }
 }
