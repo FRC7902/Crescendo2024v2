bcd9bce713417128b6a871ee099a3c4699f32eb8
aadhyasriram <152321380+aadhyasriram@users.noreply.github.com>
2024-10-17 18:47:30 -0400

    Camera Subsystem Comments
    I just made comments on the camera subsystem to make it easier to understand.

--- Crescendo2024/src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ Crescendo2024/src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -21,29 +21,43 @@ public class CameraSubsystem extends SubsystemBase {
   /** Creates a new CameraSubsystem. */
   public CameraSubsystem() {
     m_visionThread = new Thread(
+      // The thread improves efficency of multitasking: Photovision work synchronously with main
       () -> {
         UsbCamera camera = CameraServer.startAutomaticCapture();
+        // Automatic Capture
         camera.setResolution(100, 100);
+        // Set intial resolution 
 
         CvSink cvSink = CameraServer.getVideo();
+        // Puts video frames in mat element
         CvSource outputStream = CameraServer.putVideo("Rectangle", 100, 100);
-
+        // Defines "rectangle" to send the video frames to the camera server
+        
         Mat mat = new Mat();
+        // Stores image data in pixel matrix
 
         while(!Thread.interrupted()){
+          // Runs until thread interrupted
           if(cvSink.grabFrame(mat) == 0){
+            // If there is no image in the camera it returns an error
             outputStream.notifyError(cvSink.getError());
+            // Retrieves insight on image error
             continue;
+            // continues to next iteration
           }
         }
 
         Imgproc.rectangle(mat, new Point (100,100), new Point(400,400), new Scalar(255, 255, 255), 5);
+        // Rectangle drawn on mat's captured frames
         outputStream.putFrame(mat);
+        // The new processed frame is sent to output stream
 
       }
     );
     m_visionThread.setDaemon(true);
+    // The program can exit
     m_visionThread.start();
+    
 
 
   }
